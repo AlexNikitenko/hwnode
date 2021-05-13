7aca1d39863f71e19cd7d063af892b6588d9e785
AlexNikitenko <nikitenko.ae90@gmail.com>
2021-05-13 18:52:14 +0300

    2021-05-13-hw-ws-rooms

--- 2021-05-13-hw-websocket-room/routes/index.js
+++ 2021-05-13-hw-websocket-room/routes/index.js
@@ -3,29 +3,10 @@ const {
 } = require('axios');
 var express = require('express');
 var router = express.Router();
-const multer = require('multer');
-const fs = require('fs');
-
-const upload = multer({
-  limits: {
-    fileSize: 10000000000,
-  }
-});
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index');
-});
-
-router.post('/', upload.single('filename'), async (req, res) => {
-  const file = req.file;
-  console.log('>>>', file.originalName);
-
-  const writeStream = fs.createWriteStream(`uploads/${file.originalName}`);
-
-  await file.stream.pipe(writeStream);
-
-  res.send('file was sent');
 });
 
 module.exports = router;
